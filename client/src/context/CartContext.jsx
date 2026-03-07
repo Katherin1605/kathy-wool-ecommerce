babfda4d24fef8ca569c2c361411616c078e0153
Daniela Elizondo <dani.elizondo.munoz@gmail.com>
2026-03-07 01:53:57 -0300

    corrección CartContext increment and decrement

--- client/src/context/CartContext.jsx
+++ client/src/context/CartContext.jsx
@@ -1,4 +1,5 @@
 import { createContext, useContext, useState, useEffect } from "react";
+//si agregamos useLocalStorage: import {useLocalStorage} from '../hooks/useLocalStorage';
 
 const CartContext = createContext();
 
@@ -38,36 +39,36 @@ export const CartProvider = ({ children }) => {
             );
         }
 
+//agregar removeFromCart
+
         return [...prevCart, { ...product, quantity: 1 }];
         });
     };
 
     const incrementQuantity = (id) => {
         setCart((prevCart) =>
-        prevCart.map((item) =>
-            item.id === id
-            ? { ...item, quantity: item.quantity + 1 }
-            : item
-        )
+            prevCart.map((item) =>
+                item.id === id
+                ? { ...item, quantity: item.quantity + 1 }
+                : item
+            )
         );
     };
 
     const decrementQuantity = (id) => {
         setCart((prevCart) =>
-        prevCart
-            .map((item) =>
+        prevCart.map((item) =>
             item.id === id
-                ? { ...item, quantity: item.quantity - 1 }
-                : item
-            )
-            .filter((item) => item.quantity > 0)
+            ? { ...item, quantity: item.quantity - 1 }
+            : item
+        )
+        .filter((item) => item.quantity > 0)
         );
     };
 
-    const clearCart = () => {
-        setCart([]);
-    };
+    const clearCart = () => setCart([]);
 
+    //agregar en value: 
     return (
         <CartContext.Provider
         value={{
