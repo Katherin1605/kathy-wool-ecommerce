5904c37c1338cd07025c30b05d40e7147d4aad09
Josue Valdivieso <josuevaldivieso11@gmail.com>
2026-03-12 11:46:16 -0500

    variable Client

--- server/db/config.js
+++ server/db/config.js
@@ -1,18 +1,22 @@
 import pg from 'pg';
 import dotenv from 'dotenv';
-import path from 'path';
-import { fileURLToPath } from 'url';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-dotenv.config({ path: path.resolve(__dirname, '../../.env') });
+dotenv.config();
+
+const env_host = process.env.DB_HOST;
+const env_user = process.env.DB_USER;
+const env_password = process.env.DB_PASSWORD;
+const env_database = process.env.DB_NAME;
+
+
+
 
 const pool = new pg.Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    port: process.env.DB_PORT,
-    allowExitOnIdle: true
+  connectionString: process.env.DB_URL,
+  ssl: {
+    rejectUnauthorized: false
+  },
+  allowExitOnIdle: true
 });
 
 export default pool;
--- server/src/controllers/usersController.js
+++ server/src/controllers/usersController.js
@@ -3,7 +3,7 @@ import { createUserModel } from "../models/usersModel.js";
 export const registerUser = async (req, res) => {
   try{
     const { name, email, password } = req.body
-    const user = await createUserModel({ name, email, password, role: 'admin' })
+    const user = await createUserModel({ name, email, password, role: 'client' })
     return res.status(201).json( {message: 'Usuario creado correctamente', user} )
   }catch(error){
     res.status(500).json({error:'Error al crear un usuario'})
