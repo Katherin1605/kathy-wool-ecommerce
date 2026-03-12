75474fcbcc9481f91d57351a4d71b7ca109a8340
Josue Valdivieso <josuevaldivieso11@gmail.com>
2026-03-11 22:18:03 -0500

    archivo server.js

--- server/server.js
+++ server/server.js
@@ -2,12 +2,18 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
+import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/users.routes.js";
+
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.json({ message: "API funcionando 🚀" });
