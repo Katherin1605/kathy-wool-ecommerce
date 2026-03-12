0a2ad85531bd82ec7a745a42d80d115bcca61d5a
Daniela Elizondo <dani.elizondo.munoz@gmail.com>
2026-03-12 18:00:35 -0300

    actualizo server con el de Seba

--- server/server.js
+++ server/server.js
@@ -1,20 +1,14 @@
 import express from "express";
 import cors from "cors";
 import 'dotenv/config';
-import userRoutes from "./routes/userRoutes.js";
+import usersRoutes from "./routes/usersRoutes.js";
 import productsRoutes from "./routes/productsRoutes.js";
 import categoriesRoutes from "./routes/categoriesRoutes.js";
 import cartRoutes from "./routes/cartRoutes.js";
 import checkoutRoutes from "./routes/checkoutRoutes.js";
-import path from "path";
-import { fileURLToPath } from "url";
+import authRoutes from "./routes/authRoutes.js";
 
 const PORT = process.env.PORT || 3000;
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-dotenv.config({ path: path.resolve(__dirname, '../.env') });
-
-import authRoutes from "./routes/auth.routes.js";
-import userRoutes from "./routes/users.routes.js";
 
 const app = express();
 
@@ -26,14 +20,11 @@ app.use("/api", cartRoutes);
 app.use("/api", checkoutRoutes);
 
 app.use("/auth", authRoutes);
-app.use("/users", userRoutes);
+app.use("/users", usersRoutes);
 
 app.get("/", (req, res) => {
   res.json({ message: "API funcionando 🚀" });
 });
-
-
-app.use("/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`🔥 Servidor corriendo en puerto http://localhost:${PORT}`);
